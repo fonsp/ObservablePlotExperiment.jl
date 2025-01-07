3f8b330e6e8c3253fd710c02a73db1849ffa9365
Fons van der Plas <fonsvdplas@gmail.com>
2025-01-07 17:49:59 +0100

    add cell and text marks

--- src/api/marks.jl
+++ src/api/marks.jl
@@ -14,7 +14,8 @@ end
 
 export dot, dotX, dotY
 export line, lineX, lineY
-
+export cell, cellX, cellY
+export text, textX, textY
 
 
 dot(data; options...) = _call_Plot_funky(:dot, data, options)
@@ -24,4 +25,14 @@ dotY(data; options...) = _call_Plot_funky(:dotY, data, options)
 line(data; options...) = _call_Plot_funky(:line, data, options)
 lineX(data; options...) = _call_Plot_funky(:lineX, data, options)
 lineY(data; options...) = _call_Plot_funky(:lineY, data, options)
+
+
+cell(data; options...) = _call_Plot_funky(:cell, data, options)
+cellX(data; options...) = _call_Plot_funky(:cellX, data, options)
+cellY(data; options...) = _call_Plot_funky(:cellY, data, options)
+
+
+text(data; options...) = _call_Plot_funky(:text, data, options)
+textX(data; options...) = _call_Plot_funky(:textX, data, options)
+textY(data; options...) = _call_Plot_funky(:textY, data, options)
 
