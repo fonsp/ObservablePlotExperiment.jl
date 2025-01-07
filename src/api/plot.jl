06db9778005aebfb29a256a0eeb7cb8ddaa6b6ef
Fons van der Plas <fonsvdplas@gmail.com>
2025-01-07 17:50:08 +0100

    Add .plot() function to marks

--- src/api/plot.jl
+++ src/api/plot.jl
@@ -4,3 +4,16 @@ export plot
 function plot(marks...; options...)
     ObservablePlot(; marks=collect(marks), options)
 end
+
+
+
+# so you can do .plot(...) on a mark:
+function Base.getproperty(om::ObservableMark, s::Symbol)
+    if s === :plot
+        function (; kwargs...)
+            plot(om; kwargs...)
+        end
+    else
+        getfield(om, s)
+    end
+end
