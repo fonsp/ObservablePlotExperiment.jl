772f2c923413bcc81b6382414d15060b0e3969ab
Fons van der Plas <fonsvdplas@gmail.com>
2026-04-29 11:41:29 +0200

    Update example.jl

--- example.jl
+++ example.jl
@@ -1,13 +1,34 @@
 ### A Pluto.jl notebook ###
-# v0.20.4
+# v0.20.24
 
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    #! format: off
+    return quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+    #! format: on
+end
+
 # ╔═╡ 06be8ac3-fa55-449d-ad3f-8162bd36c6b7
 begin
+	# Learn more: https://plutojl.org/en/docs/packages-advanced/
 	import Pkg
-	Pkg.activate()
+	Pkg.activate(temp=true)
+	Pkg.develop(path=".")
+	Pkg.add([
+		"CSV"
+		"Dates"
+		"DataFrames"
+		"Revise"
+		"HypertextLiteral"
+	])
 end
 
 # ╔═╡ b3656548-3be2-47ed-898e-3634003eee0b
@@ -21,6 +42,144 @@ using Dates
 
 # ╔═╡ 9b80c3d6-ed15-499f-806b-6fa1091c417a
 using HypertextLiteral
+
+# ╔═╡ fec4e333-9ac1-48b1-af9e-c521e62dc5e0
+const inspector_style = @htl """
+	<style>
+.observablehq--expanded,
+.observablehq--collapsed,
+.observablehq--function,
+.observablehq--import,
+.observablehq--string:before,
+.observablehq--string:after,
+.observablehq--gray {
+  color: var(--cm-color-editor-text);
+}
+
+.observablehq--collapsed,
+.observablehq--inspect a {
+  cursor: pointer;
+}
+
+.observablehq--field {
+  text-indent: -1em;
+  margin-left: 1em;
+}
+
+.observablehq--empty {
+  color: var(--cm-color-comment);
+}
+
+.observablehq--keyword,
+.observablehq--blue {
+  color: var(--cm-color-keyword);
+}
+
+.observablehq--forbidden,
+.observablehq--pink {
+  color: var(--cm-color-comment);
+}
+
+.observablehq--orange {
+  color: var(--cm-color-tag);
+}
+
+.observablehq--null,
+.observablehq--undefined {
+  color: var(--cm-color-builtin);
+}
+	
+.observablehq--boolean {
+  color: var(--cm-color-atom);
+}
+
+.observablehq--number,
+.observablehq--bigint,
+.observablehq--date,
+.observablehq--regexp,
+.observablehq--symbol,
+.observablehq--green {
+  color: var(--cm-color-number);
+}
+
+.observablehq--index,
+.observablehq--key {
+  color: var(--cm-color-var);
+}
+
+.observablehq--prototype-key {
+  color: #aaa;
+}
+
+.observablehq--empty {
+  font-style: oblique;
+}
+
+.observablehq--string,
+.observablehq--purple {
+  color: var(--cm-color-string);
+}
+
+.observablehq--inspect {
+  font-family: Menlo, monospace;
+  overflow-x: auto;
+  display: block;
+  white-space: pre;
+}
+
+.observablehq--error .observablehq--inspect {
+  word-break: break-all;
+  white-space: pre-wrap;
+}space: pre-wrap;
+}
+	</style>
+	"""
+
+# ╔═╡ bd4bab37-46e8-4f84-a360-1d59a6e90b9b
+function debug_js_value(something_to_debug)
+	@htl """
+	$inspector_style
+	<div >
+	
+	
+	<script id="insp">
+	const { Inspector } = this?.insplib ?? await import("https://unpkg.com/@observablehq/inspector@5.0.1/src/index.js?module")
+	    
+	
+
+	const elem = this ?? document.createElement("span")
+	const insp = elem.insp ?? new Inspector(elem)
+
+	try {
+		insp.fulfilled($(ObservablePlotExperiment.smart_embed_data(something_to_debug)))
+	} catch(e) {
+		insp.rejected(e)
+		console.error(e)
+	}
+
+	elem.insp = insp
+	elem.insplib = Inspector
+	return elem
+	
+	</script>
+	</div>
+	"""
+end
+
+# ╔═╡ f89d818c-8ebd-4dbb-bca2-2e0aab2e399e
+@bind zz html"<input type=range>"
+
+# ╔═╡ 5ac00890-ef6b-456a-9463-ad88c538df09
+debug_js_value(Float64[1,2,3/2,zz])
+
+# ╔═╡ 4e6cc694-41d9-4052-818b-4e60c631144b
+with_js_lib(x) = @jsl """
+	await (async () => {
+	const Plot = this?.plotlib ?? await import("https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm");
+	return $x
+	})()
+	"""
+	
 
 # ╔═╡ b28904fd-8cc9-41fc-9806-748ede4cc6cd
 md"""
@@ -67,8 +226,19 @@ Base.loaded_modules[Base.PkgId(Base.UUID("ade2ca70-3891-5945-98fb-dc099432e06a")
 # ╔═╡ 97d30320-ff55-4d2c-90bc-935ca233f1cc
 tidyzip(x=[1,2,3], y=[6,7])
 
+# ╔═╡ d9329375-5915-4868-96e2-4d9bf5594df7
+debug_js_value(tidyzip(x=[1,2,3], y=[6,7]))
+
 # ╔═╡ 3e91edb7-3a29-4288-b8e0-826296e06d20
 vals = [x[5] for x in eachrow(d2)]
+
+# ╔═╡ a5b6e36c-574c-4921-adfd-de5ab004579c
+cell(nothing; 
+	fill=vals[1:2000], 
+	x=dayofmonth.(dates[1:2000]), 
+	y=month.(dates[1:2000]), 
+	fy=year.(dates[1:2000]),
+).x |> with_js_lib |> debug_js_value
 
 # ╔═╡ 383c54e5-9315-4b52-b77b-43f73dc252dd
 cell(zip(dates[1:400], vals[1:400]); 
@@ -211,6 +381,12 @@ plot(
 dotY(data; x=eachindex(data))
 
 # ╔═╡ Cell order:
+# ╟─fec4e333-9ac1-48b1-af9e-c521e62dc5e0
+# ╟─bd4bab37-46e8-4f84-a360-1d59a6e90b9b
+# ╠═5ac00890-ef6b-456a-9463-ad88c538df09
+# ╠═f89d818c-8ebd-4dbb-bca2-2e0aab2e399e
+# ╠═4e6cc694-41d9-4052-818b-4e60c631144b
+# ╠═a5b6e36c-574c-4921-adfd-de5ab004579c
 # ╠═d6aed7bc-972d-4916-915d-2c6adddc9768
 # ╠═04152403-abde-4b8f-83ad-1b35bc374d32
 # ╠═36a7cd4f-e957-45ab-951e-1ee90a9ad333
@@ -235,6 +411,7 @@ dotY(data; x=eachindex(data))
 # ╠═1a8a376d-6a66-4c77-b5f5-5929a1e330a5
 # ╠═52ddb495-724a-4572-aa55-0f490ac77c40
 # ╠═97d30320-ff55-4d2c-90bc-935ca233f1cc
+# ╠═d9329375-5915-4868-96e2-4d9bf5594df7
 # ╠═3e91edb7-3a29-4288-b8e0-826296e06d20
 # ╠═0c89c8ad-7ee9-4dcf-8bc3-f3a1bf3f73a4
 # ╠═b5ab10a0-d0c1-477e-8985-8cfbbc38fc6b
